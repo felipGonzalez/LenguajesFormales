7c258b3cd764c4f769be835b7dd8c68d90c71a2a
daniel hizo un comentario <daniel.lopez01@uptc.edu.co>
2019-07-26 19:17:05 -0500

    v.0.2
    expresiones regulares validadas

--- js/controller.js
+++ js/controller.js
@@ -1,40 +1,105 @@
 angular.module('AnalizadorLexico', [])
-  .controller('Controller', function ($scope, $http) {
-    var app = this;
-    app.textString = "";
-    app.listResult;
+    .controller('Controller', function($scope, $http) {
+        var app = this;
+        app.textString = "";
+        app.listResult;
+        app.verifySintaxis = function() {
+            app.listResult = [];
+            var listSplit = split(app.textString);
+            var countParentesis = 0;
+            for (let i = 0; i < listSplit.length; i++) {
+                var vecline = [];
+                if ((For.test(listSplit[i]) == true && number.test(listSplit[i]) == true) || While.test(listSplit[i]) == true) {
+                    countParentesis++;
+                    console.log(countParentesis + "for o while");
+                } else if (string.test(listSplit[i]) == true || number.test(listSplit[i]) == true || character.test(listSplit[i]) == true) {
+                    vecline = divideLine(listSplit[i]);
+                    for (let j = 0; j < vecline.length; j++) {
+                        if (typesData[vecline[j]] !== undefined) {
+                            bombeo(vecline[j + 1], i);
+                            break;
+                        }
+                    }
+                } else if (If.test(listSplit[i]) == true) {
+                    countParentesis++;
+                    console.log(countParentesis + "if");
+                } else if (derecho.test(listSplit[i]) == true) {
+                    countParentesis--;
+                    console.log(countParentesis);
+                } else {
+                    app.listResult.push(`linea ${(i + 1)}: error de sintaxis `);
+                }
+            }
+            if (countParentesis > 0) {
+                app.listResult.push(`falta parentesis derecho`);
+            } else if (countParentesis < 0) {
+                app.listResult.push(`sobra parentesis derecho`);
+            }
 
-
-
-    app.verifySintaxis = function () {
-      app.listResult = [];
-      console.log(split(app.textString));
-      var listSplit = split(app.textString);
-      for (let i = 0; i < listSplit.length; i++) {
-        if (WordRerserve[listSplit[i]] !== undefined) {
-
-          app.listResult.push(WordRerserve[listSplit[i]] + " : Es una palabra reserveda");
-
-        } else if (Operator[listSplit[i]] !== undefined) {
-
-          app.listResult.push(listSplit[i] + "  " + Operator[listSplit[i]]);
-
-        } else {
-          app.listResult.push(listSplit[i] + " : Identificador");
+            /*
+                        for (let i = 0; i < listSplit.length; i++) {
+                            if (WordRerserve[listSplit[i]] !== undefined) {
+                                app.listResult.push(WordRerserve[listSplit[i]] + " : Es una palabra reserveda");
+                            } else if (Operator[listSplit[i]] !== undefined) {
+                                app.listResult.push(listSplit[i] + "  " + Operator[listSplit[i]]);
+                            } else {
+                                app.listResult.push(listSplit[i] + " : Identificador");
+                            }
+                        }*/
         }
 
-      }
-    }
+        function split(words) {
+            var wordSplit = words.split('\n');
+            return wordSplit;
+        }
 
-    function split(words) {
-      var text = words.replace(/[\s]+/g, " ").trim();
-      var wordSplit = text.split(" ");
-      return wordSplit;
-    }
+        function divideLine(line) {
+            var text = line.replace(/[\s]+/g, " ").trim();
+            var wordSplit = text.split(" ");
+            return wordSplit;
+        }
 
+        function cutword(listSplit) {
+            var array
+            for (let i = 0; i < listSplit.length; i++) {}
 
+        }
 
+        function bombeo(word, i) {
+            console.log(i);
+            if (word.length > 2 && (WordRerserve[word] === undefined)) {
+                var indexChar = Math.floor((word.length) / 2);
+                aplyBombeo(word, indexChar, i);
+            } else if (word.length < 3) {
+                app.listResult.push(`linea ${(i + 1)}: ${word} no cumple condiciones para aplicar lema del bombeo`);
+            } else {
+                app.listResult.push(`linea ${(i + 1)}: ${word} palabra reservada del sistema`);
+            }
+        }
 
+        function aplyBombeo(word, index, j) {
+            var str = '';
+            var bombeo = "";
+            var cadena = "";
+            for (let i = 0; i < 10; i++) {
+                bombeo = setCharAt(word, index, str);
+                cadena += bombeo
+                if (WordRerserve[bombeo] !== undefined) {
+                    app.listResult.push(`linea ${(j + 1)}: La variable ${word} aplicando lema de bombeo con  n = ${i} da como resultado
+                        una palabra reservada del sistema : ${bombeo}`);
+                    app.listResult.push(cadena);
+                    break;
+                }
+                str = str + word.charAt(index);
+            }
+        }
 
-  });
+        function setCharAt(str, index, chr) {
+            if (index > str.length - 1) return str;
+            return str.substr(0, index) + chr + str.substr(index + 1);
+        }
 
+        String.prototype.replaceAt = function(index, replacement) {
+            return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+        }
+    });
--- js/reserve.js
+++ js/reserve.js
@@ -1,71 +1,84 @@
 var WordRerserve = {
-"abstract" : "abstract",
-"continue": "continue",
-"for"      : "Ciclo for",
-"new"         : "new",
-"switch": "switch",
-"assert": "assert",
-"default": "default",
-"goto": "goto",
-"package": "package",
-"synchronized": "synchronized",
-"boolean": "boolean",
-"do": "do",
-"if": "Condicional",
-"private": "private",
-"this": "this",
-"break"    : "break", 
-"double"  : "Variable numerica para numeros decimales",
-"implements": "implements",
-"protected": "protected",
-"throw": "throw",
-"byte"      : "byte",
-"else"     : "else",
-"import"      : "import",
-"public"        : "public",
-"throws": "throws",
-"case"     : "case",
-"enum": "Enumerodo",
-"instanceof" : "instanceof", 
-"return"       : "return",
-"transient": "transient",
-"catch"    : "catch",
-"extends"  : "extends",
-"int"         : "int",
-"short"         : "short",
-"try": "try",
-"char"    : "char", 
-"final"   : "final",
-"interface"  : "interface",
-"static"        : "static",
-"void": "void",
-"class"    : "class", 
-"finally" : "finally",
-"long"        : "long",
-"strictfp"      : "strictfp",
-"volatile": "volatile",
-"const"     : "const",
-"float"   : "float",
-"native"      : "native",
-"super"         : "super",
-"while": "Ciclo while",
-"String" : "Objeto da caracteres"
+    "abstract": "abstract",
+    "continue": "continue",
+    "for": "Ciclo for",
+    "new": "new",
+    "switch": "switch",
+    "assert": "assert",
+    "default": "default",
+    "goto": "goto",
+    "package": "package",
+    "synchronized": "synchronized",
+    "boolean": "boolean",
+    "do": "do",
+    "if": "Condicional",
+    "private": "private",
+    "this": "this",
+    "break": "break",
+    "double": "Variable numerica para numeros decimales",
+    "implements": "implements",
+    "protected": "protected",
+    "throw": "throw",
+    "byte": "byte",
+    "else": "else",
+    "import": "import",
+    "public": "public",
+    "throws": "throws",
+    "case": "case",
+    "enum": "Enumerodo",
+    "instanceof": "instanceof",
+    "return": "return",
+    "transient": "transient",
+    "catch": "catch",
+    "extends": "extends",
+    "int": "int",
+    "short": "short",
+    "try": "try",
+    "char": "char",
+    "final": "final",
+    "interface": "interface",
+    "static": "static",
+    "void": "void",
+    "class": "class",
+    "finally": "finally",
+    "long": "long",
+    "strictfp": "strictfp",
+    "volatile": "volatile",
+    "const": "const",
+    "float": "float",
+    "native": "native",
+    "super": "super",
+    "while": "Ciclo while",
+    "String": "Objeto da caracteres"
 }
 
 var Operator = {
-   "+":"Operador Aritmetico de suma",
-   "-":"Operador Aritmetico de resta",
-   "*":"Operador Aritmetico de multiplicacion",
-   "/":"Operador Aritmetico de divicion",
-   "%":"Operador Aritmetico Modulo"
+    "+": "Operador Aritmetico de suma",
+    "-": "Operador Aritmetico de resta",
+    "*": "Operador Aritmetico de multiplicacion",
+    "/": "Operador Aritmetico de divicion",
+    "%": "Operador Aritmetico Modulo"
 }
 
+var typesData = {
+    "String": "Es una cadena",
+    "int": "es un numero entero",
+    "double": "es un numero decimal",
+    "float": "es un numero flotante",
+    "char": "Es un caracter",
+}
 
+const string = new RegExp(/((String)\s([a-zA-Z0-9]*)(\s*=\s*"[a-zA-Z\s]*")?\s*;)/);
+const number = new RegExp(`((int|double|float)\\s([a-zA-Z0-9]*)(\\s*=\\s*[0-9]*)?\\s*;)`);
+const character = new RegExp(/((char)\s([a-zA-Z0-9]*)(\s*=\s*"[a-zA-Z\s]")?\s*;)/);
+const For = new RegExp(/(for)\s*\(((int)\s+[a-z]+\s*(=)\s*[0-9]+(;)\s*[a-z]+\s*(>|<|>=|<=|==|!=)\s*[0-9]+(;)\s*[a-z]+(\+\+|\-\-)\))\s*\{/);
+const While = new RegExp(/(while)\s*[(]([a-z]*?\s*(>|<|>=|<=|==|!=)?\s*[a-zA-Z0-9]*)[)]?\s*\{/);
+const If = new RegExp(/(if)\s*[(](\s*[a-zA-Z0-9]*\s*(>|<|>=|<=|==|!=)\s*[a-zA-Z0-9]*\s*)[)]\s*\{/);
+const derecho = new RegExp(/\}/);
 
+const variable = new RegExp(/((char)\s([a-zA-Z0-9]*)(\s?=\s?"[a-zA-Z\s]")?\s?;)/);
 
-angular.module('AnalizadorLexico', []).controller('reserve', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
-   $scope.lista = wordReserve;
-   $scope.listOperator = Operator;
-
-
+angular.module('AnalizadorLexico', []).controller('reserve', ['$scope', '$filter', '$http', function($scope, $filter, $http) {
+    $scope.lista = wordReserve;
+    $scope.listOperator = Operator;
 }]);
